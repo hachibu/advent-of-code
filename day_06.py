7168a0047f8e4b4377b70274b6a64000f30b5ae5
Raymond Sohn <raymondsohn@gmail.com>
2024-12-14 20:38:52 -0800

    Day 6 2

--- day_06.py
+++ day_06.py
@@ -8,12 +8,12 @@ def parse_maze(file):
     with open(file) as f:
         for i, line in enumerate(f.read().splitlines()):
             for j, item in enumerate(list(line)):
-                pos = (i, j)
+                p = (i, j)
                 if item == "^":
-                    start_pos = pos
-                    maze[pos] = "."
+                    start_pos = p
+                    maze[p] = "."
                 else:
-                    maze[pos] = item
+                    maze[p] = item
 
     return start_pos, maze
 
@@ -44,22 +44,48 @@ def next_direction(direction):
             return "north"
 
 
-def main():
-    start_pos, maze = parse_maze("input/day_06.txt")
-    distinct_pos = set([start_pos])
+def maze_combinations(maze):
+    for p, v in maze.items():
+        if v == ".":
+            copy = maze.copy()
+            copy[p] = "#"
+            yield copy
 
+
+def traverse(start_pos, maze):
     curr_pos = start_pos
     curr_direction = "north"
+    yield (curr_pos, ".")
     while (next_pos := next_position(curr_pos, curr_direction)) in maze:
         match maze[next_pos]:
             case ".":
-                distinct_pos.add(next_pos)
                 curr_pos = next_pos
+                yield (curr_pos, ".")
             case "#":
                 curr_direction = next_direction(curr_direction)
+                yield (curr_pos, "#")
 
-    # assert len(distinct_pos) == 41
-    assert len(distinct_pos) == 5095
+
+def main():
+    start_pos, maze = parse_maze("input/day_06_example.txt")
+
+    distinct = set([p for p, v in traverse(start_pos, maze) if v == "."])
+
+    assert len(distinct) == 41
+    # assert len(distinct) == 5095
+
+    num_loops = 0
+    for m in maze_combinations(maze):
+        visited = Counter()
+        for p, v in traverse(start_pos, m):
+            if v == "#":
+                if visited[p] > 1:
+                    num_loops += 1
+                    break
+                visited[p] += 1
+
+    assert num_loops == 6
+    # assert num_loops == 1933
 
 
 if __name__ == "__main__":
